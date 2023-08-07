01548dcc36b9f66f20ec39a665c5e7c19635b4e3
KnotEnvy <jwsnyder@gmail.com>
2023-08-07 17:02:36 -0400

    conversation ai routes, forms, inputs

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,12 +1,30 @@
 "use client";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    if (!isMounted) {
+        return null
+    }
     return(
-        <Button variant="ghost" size="icon" className="md:hidden">
-                <HamburgerMenuIcon />
-            </Button>
+        <Sheet>
+            <SheetTrigger>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                        <HamburgerMenuIcon />
+                    </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
     )
 }
 export default MobileSidebar;
