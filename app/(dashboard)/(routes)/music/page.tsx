b06292c7489f67f4bda54448ae4ae3e1cf19d09f
KnotEnvy <jwsnyder@gmail.com>
2023-08-14 19:12:23 -0400

    route error

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -38,7 +38,7 @@ const MusicPage = () => {
         try {
             setMusic(undefined);
             
-            const response = await axios.post("/api/video", values)
+            const response = await axios.post("/api/music", values)
                 
             setMusic(response.data.audio)
             form.reset();
